f6a34e08013f23d419f8f5a8a059b8af12733d57
flamingduck0 <440984240@student.sbhs.nsw.edu.au>
2023-05-17 02:31:40 +1000

    added the modified python functions for the new and improved data structure! (they are untested and invoke :despair:)

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
-import loadfile
+from flask_socketio import SocketIO, send
+
+import pythonfunctions
 import os
 
-data = loadfile.give_data()
-fencers = list(data['fencers'].keys())
+data = pythonfunctions.give_data()
+fencers = pythonfunctions.fencers(data)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ["FLASK_SECRET"]
@@ -16,6 +17,9 @@ def home_page():  # put application's code here
     output = render_template('home.html', data=data, fencers=fencers)
     return output
 
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
 
 if __name__ == '__main__':
     app.run(app)
--- loadfile.py
+++
@@ -1,8 +0,0 @@
-import json
-
-file = open('static/table1.json')
-data = json.load(file)
-
-
-def give_data():
-    return data
