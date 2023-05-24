63452fb04b87c1b9ec5817c1e43ffe8b55144f4f
flamingduck0 <440984240@student.sbhs.nsw.edu.au>
2023-05-24 21:29:12 +1000

    added the modified python functions for the new and improved data structure! (they are untested and invoke :despair:)

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask import Flask, render_template, jsonify
 
 import pythonfunctions
 import os
@@ -8,18 +7,11 @@ data = pythonfunctions.give_data()
 fencers = pythonfunctions.fencers(data)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ["FLASK_SECRET"]
-socketio = SocketIO(app)
-
 
 @app.route('/')
 def home_page():  # put application's code here
     output = render_template('home.html', data=data, fencers=fencers)
     return output
-
-@socketio.on('json')
-def handle_json(json):
-    print('received json: ' + str(json))
 
 if __name__ == '__main__':
     app.run(app)
